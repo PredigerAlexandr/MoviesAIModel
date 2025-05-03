2b68c4d17d45fa4c57c7f30bc61bea68932f6b31
Alexandr <predigeralexndr@gmail,com>
2025-05-03 13:16:03 +0400

    Init

---
+++ MoviesAIModel/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for MoviesAIModel project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MoviesAIModel.settings')
+
+application = get_wsgi_application()
