2a39182a6b2a2f2a14c3900a74a5aea0ecc83ed7
Alexandr <predigeralexndr@gmail,com>
2025-07-20 22:08:28 +0400

    v.1234

--- MoviesAIModel/myapp/urls.py
+++ MoviesAIModel/myapp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from .views import AiModel
+from django.urls import path, re_path
+from .views import *
 
 urlpatterns = [
     path('ai-model/', AiModel.as_view(), name='item-list'),
+    path('ai-model/get_preference/<str:user_id>', get_preference, name='get_preference'),
 ]
